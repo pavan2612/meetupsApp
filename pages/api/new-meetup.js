886cb685caa46db227d4d3f2266ef6260013f4da
pavan2612 <pavan2612.m@gmail.com>
2021-10-28 18:14:58 +0530

    mongodb wrong url

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,7 +4,7 @@ function handler(req,res){
     if(req.method==='POST'){
         const data = req.body
     }
-    const client = await MongoClient.connect('mongodb+srv://quizDbUser:9951122864@cluster0.p8gnz.mongodb.net/meetups?retryWrites=true&w=majority')
+    const client = await MongoClient.connect('mongodb+srv://quizDbUser:9951122@cluster0.p8gnz.mongodb.net/meetups?retryWrites=true&w=majority')
     const db = client.db()
 
     const collections = db.collection('meetup')
